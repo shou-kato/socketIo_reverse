08bf0828169617729f60498833f90e0ce0910a78
shou-kato <nanades1@gmail.com>
2021-01-10 04:01:43 +0900

    [add]注意書きを追加

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,9 @@
       <v-row align="center" justify="center">
         <v-col class="text-center">
           <v-card class="mx-auto" max-width="400" max-height="300" outlined>
+            <v-card-text
+              >適当に作ったのでところどころバグがあります。ご了承ください</v-card-text
+            >
             <v-btn text link nuxt to="/waiting-room">待機ルームへ</v-btn>
           </v-card>
         </v-col>
